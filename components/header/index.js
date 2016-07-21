440809fa09672d7383f227e06d9df7a20dc768de
Tronin Denis <i@virtuelabs.ru>
2016-07-21 16:23:59 +0300

    [#2] add pokemons list in search

--- components/header/index.js
+++ components/header/index.js
@@ -13,14 +13,29 @@ const ReactComponentWithPureRenderMixin = require('react/lib/ReactComponentWithP
 const Icon = require('react-native-vector-icons/Ionicons');
 const {Component, PropTypes} = React;
 
+import AutoComplete, {pokemon} from '../pokemons';
+
+function getImageSrcFor(name) {
+  if (!name) {
+    return null;
+  }
+  const found = pokemon.filter(p => p.name === name);
+  return found && found.length > 0 ? {
+    uri: found[0].src
+  } : null;
+}
+
+
 const {
   Animated,
   Platform,
   StyleSheet,
   View,
 	TouchableOpacity,
-	TextInput
+	TextInput,
+	Dimensions
 } = ReactNative;
+let {width, height} = Dimensions.get('window');
 
 const APPBAR_HEIGHT = Platform.OS === 'ios' ? 44 : 56;
 const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 10 : 0;
@@ -42,13 +57,17 @@ export default class AppHeader extends Component {
 		return (
 			<View style={styles.appbar}>
 				<View style={styles.center}>
-					<View style={styles.searchBar}>
-						<TextInput
-							onChange={this.props.onSearch}
-							style={styles.searchControl}
-							placeholderTextColor='rgba(255,255,255,0.5)'
-							placeholder='Enter pokemon name...'/>
-					</View>
+					<AutoComplete
+							containerStyle={styles.searchBar}
+							inputStyle={styles.searchControl}
+							inputContainerStyle={{height: APPBAR_HEIGHT -5 ,marginTop: 5, backgroundColor: 'transparent', borderColor: 'transparent'}}
+							rowStyle={styles.rowStyle}
+							onFocus={() => this.setState({isActiveField: true})}
+							onBlur={() => this.setState({isActiveField: false})}
+							getpokemonName={(name) => {
+							 this.setState({pokemonName: name});
+							 this.setState({newPointSrc: getImageSrcFor(name)});
+							}}/>
 				</View>
 				<View style={styles.left}>
 					<TouchableOpacity onPress={this.props.onLeftButtonPress}>
@@ -102,18 +121,29 @@ const styles = StyleSheet.create({
 		backgroundColor: 'rgba(255,255,255,0.1)',
 		borderRadius: 5,
 		padding: 5,
-		height: APPBAR_HEIGHT - 10
+		flex: 1,
+		width: width - 20 - 20 - 30 * 2,
+		borderColor: 'transparent'
 	},
 
 	searchControl: {
-		height: APPBAR_HEIGHT - 10,
+		position: 'relative',
 		flex: 1,
-		width: 300,
-		paddingBottom: 5,
+		marginTop: -20,
 		paddingHorizontal: 10,
 		fontSize: 16,
 		color: '#fff',
-		textAlign: 'left'
-
+		textAlign: 'left',
+		backgroundColor: 'transparent',
+		borderColor: 'transparent',
+		borderWidth: 0,
+		zIndex: 10
+	},
+	rowStyle: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginVertical: 10,
+			backgroundColor: 'transparent'
 	}
 });
--- components/pokemons/index.js
+++ components/pokemons/index.js
@@ -9,6 +9,8 @@ import pokemons from './db';
 const {width, height} = Dimensions.get('window');
 const dismissKeyboard = require('dismissKeyboard')
 
+export const pokemon = pokemons;
+
 export default class AutoComplete extends Component {
   constructor() {
     super();
@@ -23,75 +25,44 @@ export default class AutoComplete extends Component {
     pokemons: Array<any>;
   };
 
- // onTyping(text){
- //      let pokemons = names.filter(function (pokemon) {
- //          return pokemon.toLowerCase().startsWith(text.toLowerCase())
- //      }).map(function (pokemon) {
- //          return pokemon;
- //      });
- //
- //      this.setState({data: pokemons});
- //  }
   _filterData(query: string) {
     return pokemons.filter(p => p.name.toLowerCase().startsWith(query.toLowerCase()))
   }
+
+	renderPokemonListRow(pokemon) {
+		let {src, name} = pokemon;
+		let blankImg = require('./img/pokeball.png');
+		let image = src ? { uri: src } : blankImg;
+		return (
+			<TouchableOpacity onPress={() => {
+				this.setState({query: name})
+				LayoutAnimation.spring();
+				dismissKeyboard();
+				this.props.getpokemonName(name);
+			}} style={this.props.rowStyle ? this.props.rowStyle : styles.row}>
+				<Image source={image} style={{height: 30, width: 30}}/>
+				<Text style={{fontSize: 20, fontWeight: '200', marginLeft: 10}}>{name}</Text>
+			</TouchableOpacity>
+		)
+	}
 
   render() {
     const { query } = this.state;
     const data = this._filterData(query)
     return (
-        <View style={styles.container}>
-
-        <Autocomplete
-          data={data}
-          defaultValue={query}
-          onChangeText={text => {
-            LayoutAnimation.easeInEaseOut();
-            this.setState({query: text})
-          }}
-          renderItem={pokemon => (
-            <TouchableOpacity onPress={() => {
-              this.setState({query: pokemon.name})
-              LayoutAnimation.spring();
-              dismissKeyboard();
-              this.props.getpokemonName(pokemon.name);
-            }} style={styles.row}>
-              <Image source={{ uri: pokemon.src }} style={{height: 30, width: 30}}/>
-              <Text style={{fontSize: 20, fontWeight: '200', marginLeft: 10}}>{pokemon.name}</Text>
-            </TouchableOpacity>
-          )}
-        />
-          {/*<AutoCompletePlugin
-            onTyping={(text) => this.onTyping(text)}
-            onSelect={(e) => this.props.getpokemonName(e)}
-            onFocus={() => this.props.onFocus()}
-            onBlur={() => this.props.onBlur()}
-            suggestions={this.state.data}
-            placeholder='Type Monster Name'
-            style={styles.autocomplete}
-            clearButtonMode='always'
-            returnKeyType='go'
-            textAlign='center'
-            clearTextOnFocus={true}
-
-            maximumNumberOfAutoCompleteRows={10}
-            applyBoldEffectToAutoCompleteSuggestions={true}
-            reverseAutoCompleteSuggestionsBoldEffect={true}
-            showTextFieldDropShadowWhenAutoCompleteTableIsOpen={false}
-            autoCompleteTableViewHidden={false}
-
-            autoCompleteTableBorderColor='lightblue'
-            autoCompleteTableBackgroundColor='azure'
-            autoCompleteTableCornerRadius={10}
-            autoCompleteTableBorderWidth={1}
-
-            autoCompleteRowHeight={35}
-
-            autoCompleteFontSize={15}
-            autoCompleteRegularFontName='Helvetica Neue'
-            autoCompleteBoldFontName='Helvetica Bold'
-            autoCompleteTableCellTextColor={'red'}
-          />*/}
+        <View style={this.props.style}>
+	        <Autocomplete
+						containerStyle={this.props.containerStyle}
+						inputContainerStyle={this.props.inputContainerStyle}
+						style={this.props.inputStyle}
+	          data={data}
+	          defaultValue={query}
+	          onChangeText={text => {
+	            LayoutAnimation.easeInEaseOut();
+	            this.setState({query: text})
+	          }}
+	          renderItem={(pokemon) => this.renderPokemonListRow(pokemon)}
+	        />
         </View>
     );
 
